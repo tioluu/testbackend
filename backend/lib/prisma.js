bbaa262c9ec7186a9d9ff22a30c7c04b269af631
tolu <babalola.it@gmail.com>
2026-06-04 08:23:27 +0100

    change ts to js

---
+++ backend/lib/prisma.js
@@ -0,0 +1,21 @@
+// for better control as the project expands
+// import { PrismaClient } from "../src/generated/prisma/client"; 
+// import { PrismaPg } from "@prisma/adapter-pg"; 
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient; 
+// }; 
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL, 
+// }); 
+// const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     adapter, 
+//   }); 
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
+// export default prisma; 
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default prisma;
