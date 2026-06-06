7d30ae8680f6ceba82428de6e618a4768eb3a771
tolu <babalola.it@gmail.com>
2026-06-06 22:31:59 +0100

    add support for new routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,17 @@
 import "dotenv/config";
 import express from "express";
 import prisma from "../lib/prisma.js";
+import listVendor from "./routes/vendors.js";
+import homePage from "./routes/home.js";
+import listUser from "./routes/users.js";
 
 const app = express();
+
+app.use("/vendors", listVendor);
+app.use("/", homePage);
+app.use("/users", listUser )
+
 const port = 3000;
-
-app.get("/", async (req, res) => {
-  const users = await prisma.user.findMany();
-
-  res.json(users);
-});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
