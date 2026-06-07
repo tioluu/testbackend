527fbee33700cf2c85957e20f78bfa6ff32ac1de
tolu <babalola.it@gmail.com>
2026-06-07 09:25:35 +0100

    add support for auth route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,19 @@
 import "dotenv/config";
 import express from "express";
 import prisma from "../lib/prisma.js";
-import listVendor from "./routes/vendors.js";
-import homePage from "./routes/home.js";
-import listUser from "./routes/users.js";
+import vendor from "./routes/vendors.js";
+import home from "./routes/home.js";
+import user from "./routes/users.js";
+import auth from "./routes/auth.js"
 
 const app = express();
 
 app.use(express.json());
-app.use("/vendor", listVendor);
-app.use("/", homePage);
-app.use("/user", listUser )
-app.use("/create", listUser)
+app.use("/vendor", vendor);
+app.use("/", home);
+app.use("/users", user);
+app.use("/auth", auth);
+
 
 const port = 3000;
 
