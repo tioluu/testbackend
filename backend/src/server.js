9bf369331713b40c8b86c6b32308370bfbf82455
tolu <babalola.it@gmail.com>
2026-06-09 21:43:29 +0100

    add mouth path for login and register

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,15 +4,17 @@ import prisma from "../lib/prisma.js";
 import vendor from "./routes/vendorsRoute.js";
 import home from "./routes/homeRoute.js";
 import getUser from "./routes/usersRoute.js";
-import auth from "./routes/auth.js"
+import register from "./routes/authRoute.js"
+import login from "./routes/authRoute.js"
 
 const app = express();
 
 app.use(express.json());
-app.use("/", vendor);
+app.use("/api", vendor);
 app.use("/", home);
-app.use("/", getUser);
-app.use("/auth", auth);
+app.use("/api", getUser);
+app.use("/api", register);
+app.use("/api", login);
 
 
 const port = 3000;
