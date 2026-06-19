eadd33a3ee960149b62c3f82ed195fbab131605c
tolu <babalola.it@gmail.com>
2026-06-19 13:24:59 +0100

    add auth route: implement GET /me endpoint with token authentication

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,13 +1,14 @@
 import express from "express";
-import prisma from "../../lib/prisma.js";
-import bcrypt from "bcrypt";
-import { register } from "../controller/authController.js";
+import { getCurrentUser, register } from "../controller/authController.js";
 import { login } from "../controller/authController.js";
+import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post ("/register", register);
 
 router.post ("/login", login);
+
+router.get ("/me", authenticateToken, getCurrentUser);
 
 export default router;
