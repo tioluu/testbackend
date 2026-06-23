ad795cafa884e2ae74d9d3bb47ea3f6ace184152
tolu <babalola.it@gmail.com>
2026-06-23 15:21:25 +0100

    feat(userRoute): add token authentication middleware to forgot password route

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { forgot_password } from "../controller/userController.js";
+import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
