09c53eeab031f94f24790737782a29b9bf645c7d
tolu <babalola.it@gmail.com>
2026-06-30 19:18:12 +0100

    feat(productRoute): add create and delete product routes

--- backend/src/routes/productRoute.js
+++ backend/src/routes/productRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { viewProduct } from "../controller/productController.js";
+import { viewProduct,createProduct,deleteProduct } from "../controller/productController.js";
 import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get ("/view_product", viewProduct);
+router.get ("/view-product", viewProduct);
+router.post ("/create-product", authenticateToken, createProduct);
+router.delete ("/delete-product/:id", authenticateToken, deleteProduct);
 
 export default router;
