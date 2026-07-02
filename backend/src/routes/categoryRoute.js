1927653010747aac6c65ffaf957fac0758139add
tolu <babalola.it@gmail.com>
2026-07-02 07:43:52 +0100

    feat(categoryRoute): add delete category route

--- backend/src/routes/categoryRoute.js
+++ backend/src/routes/categoryRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {createCategory, viewCategory} from "../controller/categoryController.js";
+import {createCategory, viewCategory, deleteCategory} from "../controller/categoryController.js";
 
 const router = express.Router();
 
 router.post("/create-category", createCategory);
 router.get("/view-categories", viewCategory);
+router.delete("/delete-category/:id", deleteCategory);
 
 export default router;
