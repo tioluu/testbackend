5bb66e1feac22fe5df45010aa1ebfd1f9dfd28fd
tolu <babalola.it@gmail.com>
2026-07-23 15:36:39 +0100

    feat(storeRoute): add route for customizing store with authentication

--- backend/src/routes/storeRoute.js
+++ backend/src/routes/storeRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { viewStore, createStore, deleteStore } from "../controller/storeController.js";
+import { viewStore, createStore, deleteStore, customizeStore } from "../controller/storeController.js";
 import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get ("/view-store", viewStore);
 router.post ("/create-store", authenticateToken, createStore);
 router.delete ("/delete-store", authenticateToken, deleteStore);
+router.patch ("/customize-store", authenticateToken, customizeStore);
 
 
 export default router;
