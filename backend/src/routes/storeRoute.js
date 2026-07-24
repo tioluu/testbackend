37b4fbacce02c70075fa747e17ed4b916b8e2b25
tolu <babalola.it@gmail.com>
2026-07-24 17:26:07 +0100

    feat(storeRoute): add route for viewing store by ID

--- backend/src/routes/storeRoute.js
+++ backend/src/routes/storeRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { viewStore, createStore, deleteStore, customizeStore } from "../controller/storeController.js";
+import { viewStore, createStore, deleteStore, customizeStore, viewStoreById } from "../controller/storeController.js";
 import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get ("/view-store", viewStore);
+router.get ("/view-stores", viewStore);
+router.get ("/view-store/:id", viewStoreById);
 router.post ("/create-store", authenticateToken, createStore);
 router.delete ("/delete-store", authenticateToken, deleteStore);
 router.patch ("/customize-store", authenticateToken, customizeStore);
