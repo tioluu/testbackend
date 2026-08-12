de085a7dc6a7aefbc821664fecab31ee4b64b68f
tolu <babalola.it@gmail.com>
2026-08-12 15:05:16 +0100

    refactor(routes): remove unused route files

--- backend/src/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { forgot_password } from "../controllers/userController.js";
-import { authenticateToken } from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-
-router.post ("/forgot-password", forgot_password);
-
-export default router;
