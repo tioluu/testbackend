b101a40e360e85b1638e927603fda5377d1525de
tolu <babalola.it@gmail.com>
2026-08-12 15:08:01 +0100

    feat(routes): add store routes for viewing, creating, deleting, and customizing stores

---
+++ backend/src/vendorRoutes/storeRoute.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { viewStore, createStore, deleteStore, customizeStore, 
+    viewStoreById } from "../vendorControllers/storeController.js";
+import { authenticateToken } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.get ("/view-stores", viewStore);
+router.get ("/view-store/:id", viewStoreById);
+router.post ("/create-store", authenticateToken, createStore);
+router.delete ("/delete-store", authenticateToken, deleteStore);
+router.patch ("/customize-store", authenticateToken, customizeStore);
+
+
+export default router;
