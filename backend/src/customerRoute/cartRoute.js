7fe6e2d05480be5c96e2099ace40e9502f9665ae
tolu <babalola.it@gmail.com>
2026-08-12 15:06:56 +0100

    feat(routes): add cart route for viewing cart with authentication

---
+++ backend/src/customerRoute/cartRoute.js
@@ -0,0 +1,9 @@
+import express, { Router } from "express";
+import { viewCart } from "../vendorControllers/cartController.js";
+import { authenticateToken } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+ 
+router.get ("/view-cart", authenticateToken, viewCart);
+
+export default router;
